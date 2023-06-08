b0bef0fc24ae279b2b39ecb7234cc594c88fe7df
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-06-08 11:49:00 +0900

    ADD: 6월07일 비동기 문제 1차수정

--- lib/async_test/future.dart
+++ lib/async_test/future.dart
@@ -18,7 +18,7 @@ Future<void> copyText(
   try {
     final data = await File(sourcePath).readAsString();
     final text = data.replaceAll(replaceFrom, replaceTo);
-    await File('lib/async_test/furture').writeAsString(text);
+    await File('lib/async_test/sample.csv').writeAsString(text);
   } catch (err) {
     throw Exception('파일 복사 중에 오류가 발생!: $err');
   }
@@ -26,8 +26,8 @@ Future<void> copyText(
 
 void main() async {
   try {
-    await copyText('lib/async_test/furture', '김석봉', '한석봉');
-    print(await File('lib/async_test/furture').readAsString());
+    await copyText('lib/async_test/sample.csv', '김석봉', '한석봉');
+    print(await File('lib/async_test/sample.csv').readAsString());
   } catch (error) {
     print(error);
   }
