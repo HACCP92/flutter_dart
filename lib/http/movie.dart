1c8bde89b44a7137d3a10ed99e2db72e8c805d6b
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-06-09 12:33:28 +0900

    ADD: 6월09일 네트워크 문제, json 변환 수정

--- lib/http/movie.dart
+++ lib/http/movie.dart
@@ -5,19 +5,20 @@ import 'json_model/movie_json.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final movieInfo = await getMovieInfo();
-  final movieTitle = await movieInfo.map((e) => e.title).toList();
+  final movies movieInfo = await getMovies();
+  final movieTitles = await movieInfo.print(movieTitles);
 
-  print(movieTitle);
-
-  final firstMovieId = movieInfo[0].id;
+  final firstMovieId = movieInfo[298618].id;
   final movieDetail = await getMovieDetail(firstMovieId);
-  print('영화 제목 : ${movieDetail.title}');
-  print('영화 설명 : ${movieDetail.overview}');
-  print('상영 시간 : ${movieDetail.runtime}분');
+  print('영화제목 : ${movieDetail.title}');
+  print('영화설명 : ${movieDetail.overview}');
+  print('상영시간 : ${movieDetail.runtime}');
 }
 
-Future getMovieInfo() async {
+
+}
+
+Future<List<Results>> getMovieInfo() async {
   final response = await http.get(Uri.parse(
       'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
   final json = jsonDecode(response.body);
